2b657b402a5f4a8d58d00d17bf546c4d44c06323
Sergii Krivokhatko <krivokhatko@gmail.com>
2016-08-02 00:47:13 +0300

    Added functions for passwords hashing and verification.

---
+++ auth/hash.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from passlib.hash import pbkdf2_sha512
+
+
+def password_encrypt(password):
+    pass_hash = pbkdf2_sha512.encrypt(password, rounds=2000, salt_size=16)
+    return pass_hash
+
+
+def password_verify(password, pass_hash):
+    return pbkdf2_sha512.verify("password", hash)
